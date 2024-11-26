c043eec7e5f2491086791d5c4c2b640d92b5443c
xysong <xysong25@gmail.com>
2024-11-26 04:49:04 -0500

    Add: more testing instances

--- jobs/generate_tree.sh
+++ jobs/generate_tree.sh
@@ -1,5 +1,6 @@
 # Generate tree instances
 
 # Some examples
+python BinaryTreeGenerator.py -n 20 --min 5 --max 10 -o balanced/20.txt
 # python BinaryTreeGenerator.py -n 50 --min 5 --max 10 -o balanced/50.txt
 python BinaryTreeGenerator.py -n 100 --min 5 --max 10 -o balanced/100.txt
--- jobs/run.sh
+++ jobs/run.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
 # Tree instance
-instance="source/balanced/50.txt"
-out="out/balanced/50"
-mkdir -p "$out"
+# instance="source/balanced/50.txt"
+# out="out/balanced/50"
+# D_list=(200 210 220 230 240 250)
 
-D_list=(200 210 220 230 240 250)
+instance="source/balanced/20.txt"
+out="out/balanced/20"
+D_list=(80 90 100 120 140 160 180 200 220 240 260 280)
+
+mkdir -p "$out"
 # Run algo with different D
 for D in "${D_list[@]}"
 do
