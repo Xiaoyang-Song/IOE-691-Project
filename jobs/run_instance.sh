3e7f4b7eb5babd6d1bc43a33678b8ea162ed684d
xysong <xysong25@gmail.com>
2024-12-01 11:53:28 -0500

    Update: results on n=20 experiments

--- jobs/run_instance.sh
+++ jobs/run_instance.sh
@@ -2,7 +2,7 @@
 # Run on single instances (balanced, 50, 10)
 n=20
 
-D=80
+D=50
 instance="source/balanced/$n"
 out="out/balanced/$n-$D"
 mkdir -p "$out"
@@ -12,12 +12,14 @@ do
     python main.py "$instance/$i.txt" "$D" > "$out/$i.txt"
 done
 
-D=80
+python get.py -mode 'balanced' -n $n -D $D -m 100
+
 instance="source/imbalanced/$n"
 out="out/imbalanced/$n-$D"
 mkdir -p "$out"
-
 for i in {1..100}
 do
     python main.py "$instance/$i.txt" "$D" > "$out/$i.txt"
 done
+
+python get.py -mode 'imbalanced' -n $n -D $D -m 100
